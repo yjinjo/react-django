4094e1fe591361d792ba251307a89f58baa7cafb
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-05 20:55:08 +0900

    #1. Django REST Framework

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from .views import main
+from .views import RoomView
 
 
 urlpatterns = [
-    path("home", main),
-    path("", main),
+    path("room", RoomView.as_view()),
 ]
--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+
+from api.models import Room
+from api.serializers import RoomSerializer
 
 
-def main(request):
-    return HttpResponse("<h1>Hello</h1>")
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
